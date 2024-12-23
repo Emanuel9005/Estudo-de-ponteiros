cba548458e6aaf93af975adb4aa11a7ad3bd0b6c
Emanuel Campanha <emanuelcampanha@gmail.com>
2024-12-23 19:22:29 -0300

    Exercicio 5

---
+++ ExercicioPonteiro5.c
@@ -0,0 +1,27 @@
+/*Implemente a função strcpy (char *destino, char *origem) usando ponteiros.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int strcpy1(char *destino,char *origem)
+{
+    int i = 0;
+    while (*(origem + i) != '\0')
+    {
+        *(destino + i) = *(origem + i);
+        i++;
+    }
+    *(destino + i) = '\0';
+    return i;
+    
+}
+
+int main()
+{
+    char destino[25], origem[25] = {"Hello World!"};
+
+    printf("Tamanho: %d\n",strcpy1(destino,origem));
+    printf("Destino: %s\n",destino);
+
+
+    return 0;
+}
